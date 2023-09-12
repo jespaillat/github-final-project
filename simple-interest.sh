b2b042b3fd429129dce42ce06bed4de9c14834d3
jespaillat <143209788+jespaillat@users.noreply.github.com>
2023-09-11 23:10:09 -0400

    Update and rename jespaillat to simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,10 +1,10 @@
- #!/bin/bash
+a #!/bin/bash
    # This script calculates simple interest given principal,
    # annual rate of interest and time period in years.
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # jespaillat
    # Input:
    # p, principal amount
    # t, time period in years
